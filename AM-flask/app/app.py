81f5c4ed52307d729ec27f27a156d58eabfe1044
leglars.Zheng <leglars@gmail.com>
2016-06-02 01:26:19 +1000

    reset the app

--- AM-flask/app/app.py
+++ AM-flask/app/app.py
@@ -1,8 +1,10 @@
+import send_sms
+import firebase_logging as log
+
 from flask import Flask, redirect, request, render_template
+
 from firebase import firebase
 fb = firebase.FirebaseApplication("https://burning-heat-7654.firebaseio.com/", None)
-# import send_sms
-import firebase_logging as log
 
 app = Flask(__name__)
 
@@ -14,8 +16,15 @@ def hello_world():
     return render_template("index.html")
 
 
+@app.route('/sending')
+def send_message():
+
+    res = send_sms.send_message()
+    print(res)
+
+
 @app.route("/receive", methods=['GET', 'POST'])
-def hello():
+def receive():
     pass
     # resp = twilio.twiml.Response()
     # resp.message("hello, this is haha")
@@ -25,50 +34,79 @@ def hello():
 @app.route("/income", methods=['GET', 'POST'])
 def reply():
 
-    from_num = "+" + str(request.values.get('From', None)).strip()
-    # the number get from http is " 61400111222"
-    text = str(request.values.get('Body', None))
+    def logger(context_data):
+        if not context_data[from_num]:
+            send_sms.error_response_number(text, from_num)
+
+        contact = context_data[from_num]["name"]
+        user = context_data[from_num]["user"]
+
+        log_result = log.daily_logging(user, contact, text)
+        if log_result == 1:
+            pass
+        elif log_result == 2:
+            pass
+        elif not log_result:
+            # send_sms.error_response_warning(text)
+            return "error response warning"
+
+    """
+    POST /income HTTP/1.1
+    Host: 04f80d2d.ngrok.io   // testing port
+    Content-Length: 140
+    Accept-Encoding: gzip, deflate
+    Accept: */*
+    User-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.35 Safari/535.1
+    connection: close
+    X-Plivo-Signature: 1VBRa9BARNAi8GSusYTNxpE3T6U=
+    Content-Type: application/x-www-form-urlencoded
+    X-Forwarded-For: 54.177.70.197
+
+    To=61429968959&From=61478417108&TotalRate=0&Units=1&Text=How+are+you&TotalAmount=0&Type=sms&MessageUUID=ca425462-27f0-11e6-890b-22000ae90d37
+    """
+    from_num = request.values.get('From', None)
+    text = str(request.values.get('Text', None))
+
     data = fb.get("/contact", None)
 
-    # if not data[from_num]:
-    #     send_sms.error_response_number(text, from_num)
+    logger(data)
 
-    contact = data[from_num]["name"]
-    user = data[from_num]["user"]
 
-    log_result = log.daily_logging(user, contact, text)
-    if log_result == 1:
+
+    response = "we got your message: " + text
+    # res = send_sms.reply_message(from_num, response)
+    res = send_sms.send_message(from_num, response)
+
+    print(res)
+    print(type(res))
+    return "replied"
+
+
+    #
+    #
+    # # Message UUID for which the details will be retrieved
+    # params = {'message_uuid': '0936ec98-7c4c-11e4-9bd8-22000afa12b9'}
+    # # Fetch the details
+    # response = p.get_message(params)
+
+
+@app.route("/delivery_report/", methods=['GET', 'POST'])
+def report():
+    # Sender's phone number
+    from_number = request.values.get('From')
+    # Receiver's phone number - Plivo number
+    to_number = request.values.get('To')
+    # Status of the message
+    status = request.values.get('Status')
+    # Message UUID
+    uuid = request.values.get('MessageUUID')
+
+    if status != "delivered":
         pass
-    elif log_result == 2:
-        pass
-    elif not log_result:
-        # send_sms.error_response_warning(text)
-        return "error response warning"
 
-
-    # Message UUID for which the details will be retrieved
-    params = {'message_uuid': '0936ec98-7c4c-11e4-9bd8-22000afa12b9'}
-    # Fetch the details
-    response = p.get_message(params)
-
-
-    @app.route("/delivery_report/", methods=['GET', 'POST'])
-    def report():
-        # Sender's phone number
-        from_number = request.values.get('From')
-        # Receiver's phone number - Plivo number
-        to_number = request.values.get('To')
-        # Status of the message
-        status = request.values.get('Status')
-        # Message UUID
-        uuid = request.values.get('MessageUUID')
-
-        if status != "delivered":
-            pass
-
-        # Prints the status of the message
-        print ("From: %s, To: %s, Status: %s, MessageUUID: %s" % (from_number, to_number, status, uuid))
-        return "Delivery status reported"
+    # Prints the status of the message
+    print("From: %s, To: %s, Status: %s, MessageUUID: %s" % (from_number, to_number, status, uuid))
+    return "Delivery status reported"
 
 
 if __name__ == '__main__':
--- AM-flask/app/send_sms.py
+++ AM-flask/app/send_sms.py
@@ -4,7 +4,7 @@
 # auth_token = 'eaae9bf97f64e5d844d5c65247216220'
 # client = TwilioRestClient(account_sid, auth_token)
 
-import plivo
+import plivo, plivoxml
 
 auth_id = "MAMGEWZGE5NWRJNME3ZJ"
 auth_token = "ZTNiNmIyZDU1N2JjNWQ4MGM5NWE4NmI5MjM1OThm"
@@ -12,10 +12,11 @@ auth_token = "ZTNiNmIyZDU1N2JjNWQ4MGM5NWE4NmI5MjM1OThm"
 p = plivo.RestAPI(auth_id, auth_token)
 
 _ADMIN_NUMBER = "+61478417108"
-_SERVICE_NUMBER = "+61400357926"
+_SERVICE_NUMBER = "+61429968959"
+_TEST_QUESTION = "this is a test message from plivo"
 
 
-def send_message(to, question):
+def send_message(to_number=_ADMIN_NUMBER, question=_TEST_QUESTION):
     # client.messages.create(
     #     to=to,
     #     from_=_SERVICE_NUMBER,
@@ -24,13 +25,45 @@ def send_message(to, question):
 
     params = {
         'src': _SERVICE_NUMBER,
-        'dst': _ADMIN_NUMBER,
-        'text': "this is a test message from plivo",
+        'dst': to_number,
+        'text': question,
         # 'url': "http://thinkingofyou.uqcloud.net/automessage/report",
         'method': "POST"
     }
 
     response = p.send_message(params)
+    """
+    This is an example of response:
+    tuple(int, dictionary{})
+    (
+        202,
+        {
+            'message_uuid': ['3829ba15-12c8-499b-aa88-730619e17272'],
+            'message': 'message(s) queued',
+            'api_id': 'b1d5f55d-27f8-11e6-be4a-22000ae40186'
+        }
+    )
+    """
+    return response
+
+
+def reply_message(from_number, text="Thanks, we've received your message.", to_number=_SERVICE_NUMBER):
+    """
+    :param from_number: get who send this message
+    :param text: this is the default message
+    :param to_number: here, we just have 1 service number, if the app need send many messages, maybe have more than one
+                        service number, so this parameter would be useful to identify
+    """
+    params = {
+      "src": to_number,
+      "dst": from_number,
+    }
+
+    # Generate a Message XML with the details of
+    # the reply to be sent.
+    r = plivoxml.Response()
+    r.addMessage(text, **params)
+    return r.to_xml()
 
 
 def error_response_warning(error, user, name, question):
