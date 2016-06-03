804e7ee87c7f5aff45bcbcc3aed8635c53f32ae7
leglars.Zheng <leglars@gmail.com>
2016-06-04 01:09:52 +1000

    rebuild the logging module: deconstructure the logging function to meeting the new requirement

--- AM-flask/app/firebase_logging.py
+++ AM-flask/app/firebase_logging.py
@@ -1,51 +1,156 @@
 import datetime
+import re
 from firebase import firebase
 fb = firebase.FirebaseApplication("https://burning-heat-7654.firebaseio.com/", None)
 
-_YES = ["yes", "Yes", "YES"]
-_NO = ["no", "No", "NO"]
+# _YES = ["yes", "Yes", "YES", "Y", "y"]
+# _NO = ["no", "No", "NO", "N", "n"]
 
 
-def daily_logging(user, contact, text):
-    text.split(' ')
-    i = 0
-
-    query = query_builder(user, contact)
-
-    for word in text:
-        if word in _YES or word in _NO:
-            if word in _YES:
-                connected = 1
-            else:
-                connected = 0
-
-            fb.patch(query, {'connected': connected})
-            # fb.post_async(query, data)
-            return 1
-
-        elif word == "time" or word == "times":
-            time = int(text[i-1])
-            fb.patch(query, {'time': time})
-            # fb.put(query, "123", {'time': time})    #  put is work too
-            return 2
-        i += 1
-
-    return False
+def get_date():
+    """
+    just simple get today's date
+    :return <class 'datetime.date'>, 2016-06-03
+    """
+    return datetime.date.today()
 
 
-def query_builder(user, contact):
-    query = "/logging/"
-    postfix_query = user + "/" + contact + "/"
-    start_date = datetime.datetime.strptime(str(fb.get("/logging/startDate", None)), "%Y%m%d").date()
-    today = datetime.date.today()
-    delta = int(str(today - start_date).split(' ')[0])
+def get_start_date(contact):
+    """
+    get the evaluation start-date of this contact
+    :param contact: string
+    :return <class 'datetime.date'>, 2016-06-03
+    """
+    query = "/logging/response/" + contact + "/startDate"
+    return datetime.datetime.strptime(fb.get(query, None), "%Y%m%d").date()
+
+
+def str_date(date):
+    """
+    convert the datetime.date into string
+    :param date: <class 'datetime.date'>, 2016-06-03
+    :return 20160603
+    """
+    d = str(date).split('-')
+    string_date = ""
+    for i in range(len(d)):
+        string_date += d[i]
+    return string_date
+
+
+def generate_query_by_time(query, contact):
+    """
+     this function generates the time layout query based on the date
+     :param query: string; the base of query path: "/logging/response/"
+     :param contact: string
+     :return the final query path: "/logging/response/<contact>/dailyMessage/week1/day1"
+    """
+    # the format of delta: 18 days, 0:00:00
+    query += contact + "/dailyMessage/"
+
+    try:
+        delta = int(str(get_date() - get_start_date(contact)).split(' ')[0])
+    except TypeError:
+        return "the contact hasn't initialize the evaluation"
+
     if delta <= 0:
-        query += "week1/day1/" + postfix_query
+        query += "week1/day1"
     else:
         week = str(delta // 7 + 1)
         day = str(delta % 7 + 1)
-        query += "week" + week + "/day" + day + "/" + postfix_query
+        query += "week" + week + "/day" + day
     return query
+
+
+def is_new_contact(user):
+    """
+
+    :param user: string of user name
+    :return if user has data, means not a new contact, return false; if none, return true.
+    """
+    query = "/logging/response/" + str(user)
+    if fb.get(query, None):
+        return False
+    return True
+
+
+def create_contact_info(contact, user, number):
+    """
+    create the basic info of contact under /logging/response
+    :param contact: string
+    :param user: string
+    :param number: string  "+61478417108"
+    """
+
+    query = "/logging/response/" + contact
+    data = {
+        'startDate': str_date(get_date()),
+        'number': number,
+        'user': user
+    }
+
+    fb.patch(query, data)
+
+
+def text_parse(text):
+    """
+    parse the response text and split the record
+    :param text:
+    :return tuple <times(int), is_connected(boolean)>
+    """
+    is_connected = False
+    times = 0
+
+    """
+    Since the response follow that, if yes, "Y<whatever>3"; if no, "N"
+    we just need find the digit in the text
+
+    if the text does contain digit, it must be no;
+    if contains and great than 0, it must be yes.
+    """
+    match = re.search(r"\d+", text)
+    if match:
+        times = int(match.group())
+        if times > 0:
+            is_connected = True
+
+    return times, is_connected
+
+
+def daily_message_logger(contact, text):
+    base_query = "/logging/response/"
+    query = generate_query_by_time(base_query, contact)
+
+    times, is_connected = text_parse(text)
+
+    data = {
+        'date': str_date(get_date()),
+        'isResponsed': True,
+        'responseText': text,
+        'isConnected': is_connected,
+        'times': times,
+    }
+
+    fb.patch(query, data)
+
+
+def daily_logging(user, contact, number, text):
+    """
+    create the daily message logging automatically, also create the record the data based on the time changing
+    :param user: string
+    :param contact: string
+    :param number: string "+61478417108"
+    :param text: string
+    :return success update return True, or not
+    """
+    try:
+        if is_new_contact(user):
+            create_contact_info(contact, user, number)
+
+        daily_message_logger(contact, text)
+        return True
+    except:
+        return False
 
 
 def status_logging():
